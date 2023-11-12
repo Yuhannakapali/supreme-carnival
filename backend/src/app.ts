b9b1a43b4edfc1f1c9135597746a481b4a18a235
Yuhanna Kapali <yuhannakapali@gmail.com>
2023-11-12 19:17:26 +0545

    updated the server code from the index

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,13 +2,24 @@ import fastify from "fastify";
 
 export const server = fastify();
 
-server.get("/", async (request, reply) => {
+server.get("/",  (request, reply) => {
   return {
     status: 200,
     message: "ready to rock and roll!"
   };
 });
 
-server.get("/ping", async (request, reply) => {
+server.get("/ping",  (request, reply) => {
   return "pong\n";
 });
+
+
+export const runServer = (port: number = 3001) => {
+  server.listen({ port }, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+  });
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,3 @@
-import { server } from "./app";
+import { runServer } from "./app";
 
-server.listen({ port: 3001 }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
-});
+runServer()
